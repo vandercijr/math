50540bb438fd4376ded08ee4a4079bafcecd4cea
vanderci <vandercijr@gmail.com>
2020-12-11 00:10:01 -0300

    add kahan algol

---
+++ src/kahanSum.js
@@ -0,0 +1,31 @@
+// https://rosettacode.org/wiki/Kahan_summation#Python:_Arbitrary_precision_Decimal
+
+/*
+float kahanSum(float *nums, int count) {
+  float sum = 0.0f;
+  float c = 0.0f;
+  float t, y;
+  int i;
+  for (i = 0; i < count; ++i) {
+      y = nums[i] - c;
+      t = sum + y;
+      c = (t - sum) - y;
+      sum = t;
+  }
+  return sum;
+}*/
+
+export const kahanSum = (input) => {
+  let sum = 0.0,
+    c = 0.0,
+    t,
+    y;
+
+  for (let i = 0, s = input.length; i < s; i++) {
+    y = input[i] - c;
+    t = sum + y;
+    c = t - sum - y;
+    sum = t;
+  }
+  return sum;
+};
