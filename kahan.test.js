6581f50b1ae5119e9cad8c7a8fb9c33d2179940f
vanderci <vandercijr@gmail.com>
2020-12-11 00:10:45 -0300

    remove

--- kahan.test.js
+++
@@ -1,6 +0,0 @@
-describe('Kahan Summation Algorithm', () => {
-  it('should return 1.0000000000000000', () => {
-    expect(kahanSum(input)).toBeEqualTo(1.0000000000000)
-    // expect(kahanSum(input)).toBeCloseTo(1, 1)
-  })
-})
